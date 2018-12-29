0d7043971b1c33b0c8b69508e518e1239ee69172
Karl Horning <k.horning@me.com>
2018-12-29 19:35:51 +0000

    v3 cloned from v2

---
+++ v3/app.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/yelp_camp', {
+    useNewUrlParser: true
+});
+app.use(express.static('views'));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.set('view engine', 'ejs');
+
+// Schema
+const campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String,
+});
+
+const Campground = mongoose.model('Campground', campgroundSchema);
+
+// index
+app.get('/', (req, res) => {
+    res.render('landing');
+});
+
+// INDEX: show all campgrounds
+app.get('/campgrounds', (req, res) => {
+    // get all campgrounds from DB
+    Campground.find({}, (err, campgrounds) => {
+        if (err) {
+            console.error(err);
+        } else {
+            res.render('index', {
+                campgrounds: campgrounds
+            });
+        }
+    });
+});
+
+// Add campground
+app.post('/campgrounds', (req, res) => {
+    // get data from form and add to campgrounds
+    const name = req.body.name;
+    const image = req.body.image;
+    const description = req.body.description;
+    const newCampground = {
+        name: name,
+        image: image,
+        description: description
+    };
+
+    // create a new campground and save to DB
+    Campground.create(newCampground, (err, newlyCreatedCampground) => {
+        if (err) {
+            console.error(err);
+        } else {
+            // redirect back to campgrounds page
+            res.redirect('/campgrounds');
+        }
+    });
+});
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render('new');
+});
+
+// SHOW: shows info about one campground
+app.get('/campgrounds/:id', (req, res) => {
+    // find the campground with the provided ID
+    Campground.findById(req.params.id, (err, foundCampground) => {
+        if (err) {
+            console.error(log);
+        } else {
+            // render the template with that campground
+            res.render('show', {
+                campground: foundCampground
+            });
+        }
+    });
+});
+
+// Run the server
+app.listen(3000, () => console.log("The server is running on port 3000"));
