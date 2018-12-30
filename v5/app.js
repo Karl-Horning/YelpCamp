8ddcf476a9f2bfd668c7aff4a3fbc5084b806108
Karl Horning <k.horning@me.com>
2018-12-30 14:15:39 +0000

    Added public directory, added custom css in public directory, moved images to public directory

--- v5/app.js
+++ v5/app.js
@@ -11,6 +11,7 @@ mongoose.connect('mongodb://localhost/yelp_camp', {
     useNewUrlParser: true
 });
 app.use(express.static('views'));
+app.use(express.static(`${__dirname}/public`));
 app.use(bodyParser.urlencoded({
     extended: true
 }));
