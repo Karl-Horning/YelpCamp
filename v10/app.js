69df7f05f1ec29a85a36e7975813af3baa1869ac
Karl Horning <k.horning@me.com>
2019-01-05 20:29:52 +0000

    method-override added

--- v10/app.js
+++ v10/app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
+const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 const Comment = require('./models/comment');
 const User = require('./models/user');
@@ -17,12 +18,13 @@ const commentRoutes = require('./routes/comments');
 mongoose.connect('mongodb://localhost/yelp_camp', {
     useNewUrlParser: true
 });
-app.use(express.static('views'));
-app.use(express.static(`${__dirname}/public`));
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.set('view engine', 'ejs');
+app.use(express.static(`${__dirname}/public`));
+app.use(methodOverride('_method'));
+app.use(express.static('views'));
 // seedDB();
 
 // Passport configuration
