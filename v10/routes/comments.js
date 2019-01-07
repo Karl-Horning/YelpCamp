247b9250f193114943d30091f3bd7d3298e567c1
Karl Horning <k.horning@me.com>
2019-01-07 12:15:10 +0000

    Destroy route added

--- v10/routes/comments.js
+++ v10/routes/comments.js
@@ -75,6 +75,17 @@ router.put('/:comment_id', (req, res) => {
     });
 });
 
+// Comments destroy route
+router.delete('/:comment_id', (req, res) => {
+    Comment.findByIdAndRemove(req.params.comment_id, err => {
+        if (err) {
+            res.redirect('back');
+        } else {
+            res.redirect(`/campgrounds/${req.params.id}`);
+        }
+    });
+});
+
 // Middleware
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
